377eca3216fcc47e43d2fea24d64c46666604bad
Andrew Poelstra <apoelstra@wpsoftware.net>
2023-03-04 20:35:52 +0000

    clean up linking logic; add notes to every commit

--- test-pr.sh
+++ test-pr.sh
@@ -5,8 +5,8 @@ set -ex
 GIT_DIR="$(git rev-parse --path-format=absolute --git-common-dir)"
 JSON="$GIT_DIR/../../repo.json"
 OUT_DIR=/nix/var/nix/gcroots/per-user/apoelstra/ci-output
-OUT_FILE="$OUT_DIR/$1--$(date "+%FT%H%M%S")"
 
+# Parse inputs
 PRNUM=$1
 ACK=$2
 if git rev-parse --verify --quiet "$1^{commit}"; then
@@ -20,35 +20,42 @@ else
 	TARGET=checkPr
 fi
 
+# Do actual build
 banner "Testing $PRNUM"
+DRV_FILE=$(
+  nix-instantiate \
+	--arg jsonConfigFile "$JSON" \
+	--arg prNum "\"$PRNUM\"" \
+	-A "$TARGET" \
+	"$GIT_DIR/../../check-pr.nix"
+)
 
-nix-build \
-	--out-link "$OUT_FILE" \
+OUT_FILE=$(
+  nix-build \
 	--no-build-output \
+	--no-out-link \
 	--keep-failed \
 	--arg jsonConfigFile "$JSON" \
 	--arg prNum "\"$PRNUM\"" \
 	-A "$TARGET" \
 	"$GIT_DIR/../../check-pr.nix"
-
-DRV_FILE=$(
-nix-instantiate \
-	--arg jsonConfigFile "$JSON" \
-	--arg prNum "\"$PRNUM\"" \
-	--add-root "$OUT_FILE.drv" \
-	-A "$TARGET" \
-	"$GIT_DIR/../../check-pr.nix"
-)
-OUT_FILE=$(
-nix-build \
-	--no-build-output \
-	--out-link "$OUT_FILE" \
-	--arg jsonConfigFile "$JSON" \
-	--arg prNum "\"$PRNUM\"" \
-	-A "$TARGET" \
-	"$GIT_DIR/../../check-pr.nix"
 )
 
+# Add outputs to gc roots
+ln -sf "$DRV_FILE" "$OUT_DIR/"
+ln -sf "$OUT_FILE" "$OUT_DIR/"
+
+# Add git notes to every affected commit
+export GIT_NOTES_REF="refs/notes/check-pr"
+TIME=$(date "+%F %H:%M:%S")
+
+for COMMIT_DIR in "$OUT_FILE"/*; do
+	git notes append "$(basename "$COMMIT_DIR")" -m "
+$TIME: $DRV_FILE
+$TIME: $COMMIT_DIR"
+done
+
+# Ack on github, if this is what we're supposed to do
 if [ "$TARGET" == "checkPr" ]; then
 	if [ "$ACK" == "ACK" ]; then
 		gh pr review "$PRNUM" -a -b "ACK $(git rev-parse "pr/$PRNUM/head")"
@@ -56,11 +63,6 @@ if [ "$TARGET" == "checkPr" ]; then
 		echo "Not ACKing because second arg was not the literal text ACK."
 	fi
 fi
-
-TIME=$(date "+%F %H:%M:%S")
-GIT_NOTES_REF="refs/notes/check-pr" git notes append "$COMMIT_ID" -m "
-$TIME: $DRV_FILE
-$TIME: $OUT_FILE"
 
 echo "Success. Added notes to $COMMIT_ID $(git rev-parse "$COMMIT_ID")"
 
