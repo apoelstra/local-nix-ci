350595683277378edd22bb8bf42ad14ad5e24b77
Andrew Poelstra <apoelstra@wpsoftware.net>
2023-03-05 14:14:35 +0000

    update Simplicity for new scheme

--- BlockstreamResearch.Simplicity.check-pr.nix
+++ BlockstreamResearch.Simplicity.check-pr.nix
@@ -7,13 +7,12 @@
 , lib ? pkgs.lib
 , stdenv ? pkgs.stdenv
 , jsonConfigFile
-, prNum 
+, prNum
 }:
 let
   utils = import ./andrew-utils.nix {};
-  tools-nix = pkgs.callPackage utils.tools-nix-path {};
   jsonConfig = lib.trivial.importJSON jsonConfigFile;
-  gitCommits = utils.githubPrSrc {
+  gitCommits = utils.githubPrSrcs {
     # This must be a .git directory, not a URL or anything, since githubPrCommits
     # well set the GIT_DIR env variable to it before calling git commands. The
     # intention is for this to be run locally.
@@ -22,49 +21,48 @@ let
     inherit prNum;
   };
   checkData = rec {
-    projectName = jsonConfig.repoName;
-    inherit prNum;
+    name = "${jsonConfig.repoName}-pr-${builtins.toString prNum}";
+
     argsMatrices = [{
+      srcName = self: self.src.commitId;
+      mtxName = self: "${self.src.shortId}-${self.attr}";
+
       attr = [ "c" "coq" "haskell" "compcert" "vst" ];
       src = gitCommits;
     }];
-  
-    checkSingleCommit = { src, attr }:
+ 
+    singleCheckDrv = { src, attr, srcName, mtxName }: dummy:
       let
         sourceDir = src.src;
         drv = builtins.getAttr attr (import "${sourceDir}/default.nix" {});
-        tweakedDrv = if attr == "haskell"
-          then drv.overrideAttrs (self: super: {
-            postBuild = ''
-              set -ex
-              echo "Running all code-generation steps and checking output against checked-in files."
-              ./dist/build/GenPrecomputed/GenPrecomputed
-              diff "precomputed.h" ${sourceDir}/C/precomputed.h
-              rm "precomputed.h"
-  
-              ./dist/build/GenPrimitive/GenPrimitive
-              for inc in *.inc; do
-                  diff "$inc" "${sourceDir}/C/primitive/elements/$inc"
-              done
-              rm ./*.inc
-  
-              #./dist/build/GenRustJets/GenRustJets # Output not committed anywhere
-              ./dist/build/GenTests/GenTests
-              for inc in checkSigHashAllTx1.[ch]; do
-                  diff "$inc" "${sourceDir}/C/primitive/elements/$inc"
-  	    done
-  	    rm checkSigHashAllTx1.[ch]
-  
-              for inc in *.[ch]; do
-                  diff "$inc" "${sourceDir}/C/$inc"
-              done
-              rm ./*.[ch]
-            ''; })
-          else drv;
-        namedDrv = tweakedDrv.overrideAttrs (self: super: {
-          name = "simplicity-pr-" + (builtins.toString prNum) + "-" + attr + "-" + src.name;
-        });
-      in namedDrv;
+      in if attr == "haskell"
+        then drv.overrideAttrs (self: super: {
+          postBuild = ''
+            set -ex
+            echo "Running all code-generation steps and checking output against checked-in files."
+            ./dist/build/GenPrecomputed/GenPrecomputed
+            diff "precomputed.h" ${sourceDir}/C/precomputed.h
+            rm "precomputed.h"
+
+            ./dist/build/GenPrimitive/GenPrimitive
+            for inc in *.inc; do
+                diff "$inc" "${sourceDir}/C/primitive/elements/$inc"
+            done
+            rm ./*.inc
+
+            #./dist/build/GenRustJets/GenRustJets # Output not committed anywhere
+            ./dist/build/GenTests/GenTests
+            for inc in checkSigHashAllTx1.[ch]; do
+                diff "$inc" "${sourceDir}/C/primitive/elements/$inc"
+            done
+            rm checkSigHashAllTx1.[ch]
+
+            for inc in *.[ch]; do
+                diff "$inc" "${sourceDir}/C/$inc"
+            done
+            rm ./*.[ch]
+          ''; })
+        else drv;
   };
 in
 {
