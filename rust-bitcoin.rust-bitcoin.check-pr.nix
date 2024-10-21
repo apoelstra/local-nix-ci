8c461861b3f391ce0363a086fc1c25eef32b9357
Andrew Poelstra <apoelstra@wpsoftware.net>
2024-10-21 15:23:14 +0000

    update rust-bitcoin checkpr

--- rust-bitcoin.rust-bitcoin.check-pr.nix
+++ rust-bitcoin.rust-bitcoin.check-pr.nix
@@ -1,25 +1,11 @@
-{ pkgs ? import <nixpkgs> { }
-, jsonConfigFile
-, inlineJsonConfig ? null
-, inlineCommitList ? []
-, prNum
-}:
 let
   utils = import ./andrew-utils.nix { };
-  jsonConfig = if builtins.isNull inlineJsonConfig
-    then utils.parseRustConfig { inherit jsonConfigFile prNum; }
-    else inlineJsonConfig // {
-        gitCommits = map utils.srcFromCommit inlineCommitList;
-    };
-  fullMatrix = {
-    inherit prNum;
-    inherit (utils.standardRustMatrixFns jsonConfig)
-      projectName src rustc msrv lockFile
-      srcName mtxName isMainLockFile isMainWorkspace mainCargoToml
-      workspace cargoToml runDocs;#runCheckPublicApi;
-runClippy  = false;
+in import ./rust.check-pr.nix {
+  inherit utils;
+  fullMatrixOverride = {
+    #runClippy = false;
+    runFmt = false;
 
-    features1 = [ [ "default" ] ];
     features = { src, cargoToml, workspace, ... }:
       if workspace == "bitcoin"
       then utils.featuresForSrc { exclude = [ "actual-serde" ]; } { inherit src cargoToml; }
@@ -31,19 +17,5 @@ runClippy  = false;
         exclude = [ "actual-serde" "schemars" ];
       } { inherit src cargoToml; }
       else utils.featuresForSrc { } { inherit src cargoToml; };
-
-      runFmt = false;
   };
-
-  checkData = rec {
-    name = "${jsonConfig.projectName}-pr-${builtins.toString prNum}";
-    argsMatrix = fullMatrix;
-    singleCheckDrv = utils.crate2nixSingleCheckDrv;
-    memoGeneratedCargoNix = utils.crate2nixMemoGeneratedCargoNix;
-    memoCalledCargoNix = utils.crate2nixMemoCalledCargoNix;
-  };
-in
-{
-  checkPr = utils.checkPr checkData;
-  checkHead = utils.checkPr checkData;
 }
