0ae0684b0b70415e01f167400cd9e3fa76bfeb57
Andrew Poelstra <apoelstra@wpsoftware.net>
2024-11-27 12:55:03 +0000

    add corepc derivation

---
+++ rust-bitcoin.corepc.check-pr.nix
@@ -0,0 +1,20 @@
+let
+  utils = import ./andrew-utils.nix { };
+in import ./rust.check-pr.nix {
+  inherit utils;
+  fullMatrixOverride = {
+/*
+    2024-11-11 -- `integration_test` workspace is excluded entirely
+    extraTestPostRun = { workspace, ... }: if workspace == "integration_test"
+      then ''
+        set -x
+        export BITCOIND_PATH=$BITCOIND_EXE
+        export PATH=${pkgs.psmisc}/bin:${pkgs.valgrind}/bin:$PATH # for killall
+        sed -i 's/cargo run/valgrind .\/target\/debug\/integration_test/' run.sh
+        cat run.sh
+        ./run.sh
+      ''
+      else "";
+*/
+   };
+}
