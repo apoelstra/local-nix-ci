7f927a770f3e06509f65f6400ab1abd64facecf0
Andrew Poelstra <apoelstra@wpsoftware.net>
2024-11-27 01:03:36 +0000

    update elements-miniscript derivation

--- ElementsProject.elements-miniscript.check-pr.nix
+++ ElementsProject.elements-miniscript.check-pr.nix
@@ -1,33 +1,9 @@
-{ pkgs ? import <nixpkgs> { }
-, jsonConfigFile
-, prNum
-}:
 let
   utils = import ./andrew-utils.nix { };
-  jsonConfig = utils.parseRustConfig { inherit jsonConfigFile prNum; };
-  fullMatrix = {
-    inherit prNum;
-    inherit (utils.standardRustMatrixFns jsonConfig)
-      projectName src rustc msrv lockFile srcName mtxName
-      isMainLockFile isMainWorkspace mainCargoToml cargoToml
-      workspace
-      features # Must be overridden if there are any exceptional feature combinations
-      runClippy;
-#      runDocs;
-      runDocs = false; # 2024-09-24 -- should fix, several bugs
-
+in import ./rust.check-pr.nix {
+  inherit utils;
+  fullMatrixOverride = {
+    runDocs = false; # 2024-09-24 -- should fix, several bugs
     runFmt = false; # we don't do rustfmt here
   };
-
-  checkData = rec {
-    name = "${jsonConfig.projectName}-pr-${builtins.toString prNum}";
-    argsMatrix = fullMatrix;
-    singleCheckDrv = utils.crate2nixSingleCheckDrv;
-    memoGeneratedCargoNix = utils.crate2nixMemoGeneratedCargoNix;
-    memoCalledCargoNix = utils.crate2nixMemoCalledCargoNix;
-  };
-in
-{
-  checkPr = utils.checkPr checkData;
-  checkHead = utils.checkPr checkData;
 }
