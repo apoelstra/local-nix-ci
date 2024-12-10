b1686fca2736a2c0d466a968984f44af991851bf
Andrew Poelstra <apoelstra@wpsoftware.net>
2024-12-10 18:37:37 +0000

    disable clippy on simfony

--- BlockstreamResearch.s-lang.check-pr.nix
+++ BlockstreamResearch.s-lang.check-pr.nix
@@ -1,5 +1,6 @@
 import ./rust.check-pr.nix {
   fullMatrixOverride = {
       runDocs = false; # not working with slang right now
+      runClippy = false; # 2024-12-10 https://github.com/BlockstreamResearch/simfony/pull/102
   };
 }
