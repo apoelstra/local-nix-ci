83541e0991629b94b5856c08aac1ba52942e19ec
Andrew Poelstra <apoelstra@wpsoftware.net>
2024-12-24 18:28:13 +0000

    improve label-pr a bit
    Makes it more flexible regarding different remote URL formats

--- label-pr.sh
+++ label-pr.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 URL=$(git remote get-url origin)
-PULL_URL=$(echo "$URL" | sed 's/.git$/\/pull\//')
+PULL_URL=$(echo "$URL" | sed 's#\(/\)*\(.git\)*$#/pull/#')
 
 ~/code/rsgit/master/target/release/label-pr "pr:master:$PULL_URL"
 
