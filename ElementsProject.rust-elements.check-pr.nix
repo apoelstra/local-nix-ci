3f79a207d3cf6f3893d04874f07329a47a69b27a
Andrew Poelstra <apoelstra@wpsoftware.net>
2025-02-12 16:59:52 +0000

    update rust-elements for new local CI scheme

--- ElementsProject.rust-elements.check-pr.nix
+++ ElementsProject.rust-elements.check-pr.nix
@@ -1,30 +1,8 @@
-{ pkgs ? import <nixpkgs> { }
-, jsonConfigFile
-, prNum
-}:
 let
   utils = import ./andrew-utils.nix { };
-  jsonConfig = utils.parseRustConfig { inherit jsonConfigFile prNum; };
-  fullMatrix = {
-    inherit prNum;
-    inherit (utils.standardRustMatrixFns jsonConfig)
-      projectName src rustc msrv lockFile srcName mtxName
-      isMainLockFile isMainWorkspace mainCargoToml workspace cargoToml
-      features # Must be overridden if there are any exceptional feature combinations
-      runClippy
-      runDocs;
+in import ./rust.check-pr.nix {
+  inherit utils;
+  fullMatrixOverride = {
     runFmt = false; # not enabled on rust-elements
-  };
-
-  checkData = rec {
-    name = "${jsonConfig.projectName}-pr-${builtins.toString prNum}";
-    argsMatrix = fullMatrix;
-    singleCheckDrv = utils.crate2nixSingleCheckDrv;
-    memoGeneratedCargoNix = utils.crate2nixMemoGeneratedCargoNix;
-    memoCalledCargoNix = utils.crate2nixMemoCalledCargoNix;
-  };
-in
-{
-  checkPr = utils.checkPr checkData;
-  checkHead = utils.checkPr checkData;
+   };
 }
