32c9cb402f316e39c4b9b25ec2c0b0f28d89d04b
Andrew Poelstra <apoelstra@wpsoftware.net>
2025-08-24 12:18:09 +0000

    Attempt to get simplicity-webide working
    The issue here is that the MSRV of simplicity-webide is 1.78 (I think) which
    uses the new "version 4" lockfiles, which are identical to "version 3" except
    that they've changed one character so that Rust 1.77- won't parse them.
    
    However, Rust 1.77+ changed the output of cargo-metadata so that crate2nix
    does not work with it.
    
    No doubt this has been fixed upstream in crate2nix, but probably those updates
    have broken stuff that I need..

--- BlockstreamResearch.simplicity-webide.check-pr.nix
+++ BlockstreamResearch.simplicity-webide.check-pr.nix
@@ -1,1 +1,9 @@
-import ./rust.check-pr.nix {}
+let
+  utils = import ./andrew-utils.nix { };
+in import ./rust.check-pr.nix {
+  inherit utils;
+  fullMatrixOverride = {
+    # No clippy.toml; manually set MSRV
+    rustc = { src, ... }: utils.rustcsForSrc { inherit src; msrvVersion = "1.78.0"; };
+   };
+}
